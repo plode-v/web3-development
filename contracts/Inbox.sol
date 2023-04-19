1558aeec511e62d32834dd7b542f85fc42f3f54d
Plode <84858705+plode-v@users.noreply.github.com>
2023-04-19 12:38:23 +0700

    add compile.js deploy.js test and Inbox.sol contract.

---
+++ contracts/Inbox.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+contract Inbox {
+    string public message;
+
+    constructor(string memory initialMessage) {
+        message = initialMessage;
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+
+    
+}
